00dc9ca8676871569c768c4d817d4672b9b99927
Victória <126576164+MabyllyMarczal@users.noreply.github.com>
2023-08-23 21:57:06 -0300

    contagem dos algarismos e vogais
    Forma Basica de contagem dos algarismos de 0 a 9 e a exibição das vogais em um display

---
+++ contagem_e_vogais.ino
@@ -0,0 +1,356 @@
+void setup() {
+pinMode(13, OUTPUT);
+pinMode(12, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(10, OUTPUT);
+pinMode(9, OUTPUT);
+pinMode(8, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(6, OUTPUT);
+}
+
+void loop() {
+
+  //numero 0
+
+  digitalWrite(12, HIGH);
+  digitalWrite(11, HIGH); 
+  digitalWrite(10, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(12, LOW); 
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);  
+  digitalWrite(7, LOW);
+
+  
+  //numero 1
+
+  
+  digitalWrite(10, HIGH);
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(10, LOW);
+  digitalWrite(7, LOW);
+
+
+  //numero 2
+
+  
+  digitalWrite(13, HIGH); 
+  digitalWrite(8, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(9, HIGH);
+  delay(500);
+  digitalWrite(13, LOW); 
+  digitalWrite(8, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+
+
+  //numero 3
+
+  
+  digitalWrite(13, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(7, LOW);
+
+
+  //numero 4
+  
+  
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(7, LOW);
+
+
+  //numero 5
+
+  
+  digitalWrite(13, HIGH); 
+  digitalWrite(12, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  delay(500);
+  digitalWrite(13, LOW); 
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(6, LOW);
+
+  
+  //numero 6
+
+
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH); 
+  digitalWrite(11, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(13, LOW); 
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);  
+  digitalWrite(7, LOW);
+
+
+  //numero 7
+
+
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(7, LOW);
+
+
+  //numero 8
+
+
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH); 
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(13, LOW); 
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);  
+  digitalWrite(8, LOW);
+  digitalWrite(7, LOW);
+
+
+  //numero 9
+
+
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH); 
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  delay(500);
+  digitalWrite(13, LOW); 
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(7, LOW);  
+  digitalWrite(8, LOW);
+
+
+  //Animacao de término
+  //primeira volta
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+  delay(100);
+  digitalWrite(12, LOW);  
+  digitalWrite(11, HIGH);
+  delay(100);
+  digitalWrite(11, LOW);  
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);  
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);  
+  digitalWrite(8, HIGH);
+  delay(100);
+  digitalWrite(8, LOW);  
+  digitalWrite(7, HIGH);
+  delay(100);
+  digitalWrite(7, LOW);
+
+ // segunda volta
+
+    
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW); 
+  digitalWrite(12, HIGH);
+  delay(100);
+  digitalWrite(12, LOW);  
+  digitalWrite(11, HIGH);
+  delay(100);
+  digitalWrite(11, LOW); 
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);  
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);  
+  digitalWrite(8, HIGH);
+  delay(100);
+  digitalWrite(8, LOW);  
+  digitalWrite(7, HIGH);
+  delay(100);
+  digitalWrite(7, LOW);  
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+
+
+  //vogais alfabeto
+
+  
+  // Letra A
+
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH); 
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(13, LOW); 
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);  
+  digitalWrite(7, LOW);
+
+
+  // Letra E
+
+  
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH); 
+  digitalWrite(11, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, HIGH);
+  delay(500);
+  digitalWrite(13, LOW); 
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW); 
+
+
+  //Letra I 
+
+
+  digitalWrite(12, HIGH);
+  digitalWrite(9, HIGH); 
+  delay(500);
+  digitalWrite(12, LOW); 
+  digitalWrite(19, LOW);
+
+
+  //Letra O
+
+
+  digitalWrite(12, HIGH);
+  digitalWrite(11, HIGH); 
+  digitalWrite(10, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(12, LOW); 
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);  
+  digitalWrite(7, LOW);
+
+
+  //Letra U
+
+
+  digitalWrite(12, HIGH);
+  digitalWrite(10, HIGH); 
+  digitalWrite(9, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(12, LOW); 
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(7, LOW);  
+
+  //Animacao de término
+  //primeira volta
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+  delay(100);
+  digitalWrite(12, LOW);  
+  digitalWrite(11, HIGH);
+  delay(100);
+  digitalWrite(11, LOW);  
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);  
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);  
+  digitalWrite(8, HIGH);
+  delay(100);
+  digitalWrite(8, LOW);  
+  digitalWrite(7, HIGH);
+  delay(100);
+  digitalWrite(7, LOW);
+
+ // segunda volta
+
+    
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW); 
+  digitalWrite(12, HIGH);
+  delay(100);
+  digitalWrite(12, LOW);  
+  digitalWrite(11, HIGH);
+  delay(100);
+  digitalWrite(11, LOW); 
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);  
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);  
+  digitalWrite(8, HIGH);
+  delay(100);
+  digitalWrite(8, LOW);  
+  digitalWrite(7, HIGH);
+  delay(100);
+  digitalWrite(7, LOW);  
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+
+  
+}
